f9af1d412485ec3287ef5428688d9e17ed7ba49f
Janjanny <janjanbeboso0912@gmail.com>
2024-08-03 00:11:00 +0800

    add project comp

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectCard = ({img, title, desc, githubLink, projectLink}) => {
+  return (
+    <div className=' h-max bg-button-color'>
+        Project
+    </div>
+  )
+}
+
+export default ProjectCard
--- src/page/Projects.jsx
+++ src/page/Projects.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import ProjectCard from "../components/ProjectCard";
 
 const Projects = () => {
+
+  const projectList = [
+    {
+      img: '',
+      title: 'Project Title',
+      desc: 'Project Description',
+      githubLink: '',
+      websiteLink: ''
+    }
+  ]
+
   return (
     <>
-      <div className="w-full h-screen bg-bg-color text-white flex justify-center items-center pt-[4rem] ">
-        Projects
+      <div className="w-full h-screen bg-bg-color text-white flex justify-center pt-[8rem] ">
+        <div className="w-[48rem] grid grid-cols-1">
+        <ProjectCard/>
+        </div>
       </div>
     </>
   );
