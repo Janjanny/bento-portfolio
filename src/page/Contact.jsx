209100c90119bb0060231fb2d227a49f18022f58
Janjanny <janjanbeboso0912@gmail.com>
2024-08-09 22:36:09 +0800

    add stylings

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -24,8 +24,8 @@ const Contact = () => {
   return (
     <>
       <div className="w-full h-screen bg-bg-color text-white flex justify-center items-center pt-[4rem]">
-        <form ref={form} onSubmit={sendEmail} className="flex flex-col bg-grid-color p-6 gap-4">
-         
+        <form ref={form} onSubmit={sendEmail} className="flex flex-col bg-grid-color p-8 gap-6 rounded-lg border border-stroke-color w-[28rem] h-[28rem]">
+         <p className="font-[600] text-xl">Contact Me</p>
         <div class="relative w-full min-w-[200px] h-10">
     <input
       class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-white border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
@@ -42,15 +42,15 @@ const Contact = () => {
     </label>
   </div>
 
-  <div class="relative w-full min-w-[200px] h-10">
+  <div class="relative w-full min-w-[200px]">
     <textarea
       class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-white border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
-      placeholder=" " name="message" /><label
+      placeholder=" " name="message" rows={5} /><label
       class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-white peer-focus:text-white before:border-white peer-focus:before:!border-white after:border-white peer-focus:after:!border-white">Message
     </label>
   </div>
 
-          <input type="submit" value="Send" className="" />
+          <input type="submit" value="Send Email" className="bg-button-color text-sm w-full py-2 cursor-pointer rounded border border-stroke-color hover:border-primary-color hover:shadow hover:shadow-primary-color transition-all ease-in-out active:bg-[#383838] font-[600]" />
         </form>
       </div>
     </>
