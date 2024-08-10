79e36e121ef453f874427ca4cec68914d5ac0f81
Janjanny <janjanbeboso0912@gmail.com>
2024-08-10 21:36:50 +0800

    abandon

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -9,9 +9,8 @@ const Contact = () => {
   const form = useRef();
 
   const [success, setSuccess] = useState(false);
-  const [failed, setFailed] = useState(false)
-  let errorText = ""
-  
+  const [failed, setFailed] = useState(false);
+  let errorText = "";
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -22,47 +21,96 @@ const Contact = () => {
       })
       .then(
         () => {
-          setSuccess(true)
+          setSuccess(true);
         },
         (error) => {
-          setFailed(true)
-          errorText = error.text
+          setFailed(true);
+          errorText = error.text;
         },
       );
   };
 
   return (
     <>
-    {success && <Alert className="absolute mt-3 w-11/12 left-[50%] translate-x-[-50%]" icon={<IconContext.Provider value={{size: "1.5rem", color: '#20E050'}}><FaRegCheckCircle /></IconContext.Provider> } open={success} onClose={() => setSuccess(!success)}> Email sent successfully!</Alert>}
-    {failed && <Alert className="absolute mt-3 w-11/12 left-[50%] translate-x-[-50%]" icon={<IconContext.Provider value={{size: "1.5rem", color: '#20E050'}}><FaRegCheckCircle /></IconContext.Provider>} open={failed}  onClose={() => setFailed(!failed)}>Error sending email: {errorText}</Alert> }
+      {success && (
+        <Alert
+          className="absolute mt-3 w-11/12 left-[50%] translate-x-[-50%]"
+          icon={
+            <IconContext.Provider value={{ size: "1.5rem", color: "#20E050" }}>
+              <FaRegCheckCircle />
+            </IconContext.Provider>
+          }
+          open={success}
+          onClose={() => setSuccess(!success)}
+        >
+          {" "}
+          Email sent successfully!
+        </Alert>
+      )}
+      {failed && (
+        <Alert
+          className="absolute mt-3 w-11/12 left-[50%] translate-x-[-50%]"
+          icon={
+            <IconContext.Provider value={{ size: "1.5rem", color: "#20E050" }}>
+              <FaRegCheckCircle />
+            </IconContext.Provider>
+          }
+          open={failed}
+          onClose={() => setFailed(!failed)}
+        >
+          Error sending email: {errorText}
+        </Alert>
+      )}
       <div className="w-full h-screen bg-bg-color text-white flex justify-center items-center pt-[4rem]">
-        <form ref={form} onSubmit={sendEmail} className="flex flex-col bg-grid-color p-8 gap-6 rounded-lg border border-stroke-color w-[28rem] h-[28rem]">
-         <p className="font-[600] text-xl">Contact Me</p>
-        <div class="relative w-full min-w-[200px] h-10">
-    <input
-      class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-white border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
-      placeholder=" " name="user_name" required /><label
-      class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-white leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-white transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-white peer-focus:text-white before:border-white peer-focus:before:!border-white after:border-white peer-focus:after:!border-white">Name
-    </label>
-  </div>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="flex flex-col bg-grid-color p-8 gap-6 rounded-lg border border-stroke-color w-[28rem] h-[28rem]"
+        >
+          <p className="font-[600] text-xl">Contact Me</p>
+          <div class="relative w-full min-w-[200px] h-10">
+            <input
+              class="peer w-full h-full bg-transparent text-white font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-white border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
+              placeholder=" "
+              name="user_name"
+              required
+            />
+            <label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-white transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-white peer-focus:text-white before:border-white peer-focus:before:!border-white after:border-white peer-focus:after:!border-white">
+              Name
+            </label>
+          </div>
 
-  <div class="relative w-full min-w-[200px] h-10">
-    <input
-      class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-white border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
-      placeholder=" " name="user_name" required /><label
-      class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-white leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-white transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-white peer-focus:text-white before:border-white peer-focus:before:!border-white after:border-white peer-focus:after:!border-white">Email
-    </label>
-  </div>
+          <div class="relative w-full min-w-[200px] h-10">
+            <input
+              class="peer w-full h-full bg-transparent text-white font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-white border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
+              placeholder=" "
+              name="user_name"
+              required
+            />
+            <label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-white transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-white peer-focus:text-white before:border-white peer-focus:before:!border-white after:border-white peer-focus:after:!border-white">
+              Email
+            </label>
+          </div>
 
-  <div class="relative w-full min-w-[200px]">
-    <textarea
-      class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-white border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
-      placeholder=" " name="message" rows={5} required /><label
-      class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-white leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-white transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-white peer-focus:text-white before:border-white peer-focus:before:!border-white after:border-white peer-focus:after:!border-white">Message
-    </label>
-  </div>
+          <div class="relative w-full min-w-[200px]">
+            <textarea
+              class="peer w-full h-full bg-transparent text-white font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-white border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
+              placeholder=" "
+              name="message"
+              rows={5}
+              required
+              style={{ resize: "none" }}
+            />
+            <label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-white transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-white peer-focus:text-white before:border-white peer-focus:before:!border-white after:border-white peer-focus:after:!border-white">
+              Message
+            </label>
+          </div>
 
-          <input type="submit" value="Send Email" className="bg-button-color text-sm w-full py-2 cursor-pointer rounded border border-stroke-color hover:border-primary-color hover:shadow hover:shadow-primary-color transition-all ease-in-out active:bg-[#383838] font-[600]" />
+          <input
+            type="submit"
+            value="Send Email"
+            className="bg-button-color text-sm w-full py-2 cursor-pointer rounded border border-stroke-color hover:border-primary-color hover:shadow hover:shadow-primary-color transition-all ease-in-out active:bg-[#383838] font-[600]"
+          />
         </form>
       </div>
     </>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -9,24 +9,24 @@ import TechSkills from "../components/TechSkills";
 const Home = () => {
   return (
     <>
-      <div className="w-full h-screen bg-bg-color text-white flex justify-center items-center pt-[4rem] ">
-        <div className="container grid grid-cols-10 grid-rows-8 gap-[.30rem] w-[48rem] h-[31rem]">
-          <div className=" bg-grid-color border border-stroke-color rounded-md col-span-3 row-span-6 p-[1.2rem]">
+      <div className="w-full min-h-screen h-fit bg-bg-color text-white flex justify-center items-center pt-[4rem] ">
+        <div className="container grid grid-cols-10 grid-rows-8 gap-[.30rem] w-[25rem] sm:w-[39rem] md:[40rem] lg:w-[48rem] h-[31rem]">
+          <div className=" bg-grid-color border border-stroke-color rounded-md lg:col-span-3 lg:row-span-6 p-[1.2rem]">
             <WorkExperience />
           </div>
-          <div className=" bg-grid-color border border-stroke-color rounded-md col-span-7 row-span-2 p-[1.2rem]">
+          <div className=" bg-grid-color border border-stroke-color rounded-md lg:col-span-7 row-span-2 p-[1.2rem]">
             <AboutMe />
           </div>
-          <div className=" bg-grid-color border border-stroke-color rounded-md col-span-5 row-span-4 p-[1.2rem]">
+          <div className=" bg-grid-color border border-stroke-color rounded-md lg:col-span-5 lg:row-span-4 p-[1.2rem]">
             <Profile />
           </div>
-          <div className=" bg-grid-color border border-stroke-color rounded-md col-span-2 row-span-4 p-[.8rem]">
+          <div className=" bg-grid-color border border-stroke-color rounded-md lg:col-span-2 lg:row-span-4 p-[.8rem]">
             <TechSkills />
           </div>
-          <div className=" bg-grid-color border border-stroke-color rounded-md col-span-3 row-span-2 p-[1.2rem]">
+          <div className=" bg-grid-color border border-stroke-color rounded-md lg:col-span-3 lg:row-span-2 p-[1.2rem]">
             <Quotes />
           </div>
-          <div className=" bg-grid-color border border-stroke-color rounded-md col-span-7 row-span-2 p-[1.2rem]">
+          <div className=" bg-grid-color border border-stroke-color rounded-md lg:col-span-7 lg:row-span-2 p-[1.2rem]">
             <Education />
           </div>
         </div>
